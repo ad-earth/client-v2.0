c79dcc74c3b5b1e34343f602d8b86e667a62795b
sol <osunpine@gmail.com>
2023-03-02 04:32:30 +0900

    feat. 공통 컴포넌트 개발
    공통으로 재사용할 상품 썸네일 카드 컴포넌트와 뱃지 컴포넌트

---
+++ src/style/badge.style.ts
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components';
+import theme from '../shared/style/theme';
+import { BadgeType } from '../components/common/Badge';
+
+export const Container = styled.div`
+  display: flex;
+  margin: 10px 2px;
+  width: auto;
+  box-sizing: border-box;
+  font-size: inherit;
+`;
+
+export const BadgeBox = styled.div<BadgeType>`
+  ${({ type }) => {
+    if (type === 'NEW') {
+      return css`
+        background-color: ${theme.bg16};
+        color: ${theme.fc01};
+        border: 1px solid ${theme.ls16};
+      `;
+    } else if (type === 'BEST') {
+      return css`
+        background-color: ${theme.bg01};
+        color: ${theme.fc19};
+        border: 1px solid ${theme.ls07};
+      `;
+    } else if (type === 'SALE') {
+      return css`
+        background-color: ${theme.bg17};
+        color: ${theme.fc01};
+        border: 1px solid ${theme.ls17};
+      `;
+    } else if (type === 'SOLDOUT' || 'AD') {
+      return css`
+        background-color: ${theme.bg12};
+        color: ${theme.fc01};
+        border: 1px solid ${theme.ls12};
+      `;
+    }
+  }};
+  font-size: ${theme.fs11};
+  font-weight: 400;
+  text-align: center;
+  padding: 2px 8px;
+`;
