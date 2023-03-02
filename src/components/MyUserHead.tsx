a48009ae81d59f02459016ec6fa3de1058881d8e
whl5105 <73993670+whl5105@users.noreply.github.com>
2023-03-02 16:18:30 +0900

    fix. 스토리지 유저정보 문제 해결
    문제 : u_Name,u_Img 별도의 키값을 통한 유저정보 요청으로 인한 이슈
    해결 : localStorage.getItem(userInfo)  수정

--- src/components/MyUserHead.tsx
+++ src/components/MyUserHead.tsx
@@ -4,21 +4,25 @@ import { useViewport } from '../hooks/useViewport';
 import GlobalModal from './common/GlobalModal';
 import MyUserInfoModal from './MyUserInfoModal';
 
+interface UserType {
+  u_Img: string;
+  u_Name: string;
+}
+
 export default function MyUserHeadInfo() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const viewport = useViewport();
-  const userName: string = localStorage.getItem('u_Name');
-  const userImg: string = localStorage.getItem('u_Img');
+  const userInfo: UserType = JSON.parse(localStorage.getItem('userInfo'));
 
   /** 정보수정 모달 */
-  const userInfo = isModalOpen && (
+  const userModal = isModalOpen && (
     <GlobalModal onClose={() => setIsModalOpen(false)}>
       <MyUserInfoModal />
     </GlobalModal>
   );
   return (
     <t.HeadContent>
-      {userInfo}
+      {userModal}
       <t.HeadBox>
         <t.AvatarImg
           sx={
@@ -26,11 +30,11 @@ export default function MyUserHeadInfo() {
               ? { width: 56, height: 56 }
               : { width: 85, height: 85 }
           }
-          src={`${userImg ?? 'broken-image.jpg'}`}
+          src={`${userInfo.u_Img ?? 'broken-image.jpg'}`}
           onClick={() => setIsModalOpen(!isModalOpen)}
         />
         <t.Info>
-          안녕하세요 <strong>{`${userName ?? '지구'}`}님</strong>
+          안녕하세요 <strong>{`${userInfo.u_Name ?? '지구'}`}님</strong>
           <br />
           오늘도 같이 지구를 살려봅시다 🌍
         </t.Info>
