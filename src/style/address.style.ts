b27a067b55c4cbbf999bd380fb09263a2f95a630
damiiya <ddam2lee@gmail.com>
2023-03-04 15:14:23 +0900

    feat. address 컴포 개발

---
+++ src/style/address.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+export const PostWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 14px;
+`;
