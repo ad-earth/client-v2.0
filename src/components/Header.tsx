0e3bf726f3f7b6e9d605827a94ab56b77673a169
damiiya <ddam2lee@gmail.com>
2023-03-04 21:07:01 +0900

    feat. 헤더 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import * as t from '../style/header.style';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import headLogo from '../assets/headLogo.png';
 import whiteLogo from '../assets/whiteLogo.png';
 import SearchBar from './SearchBar';
@@ -6,23 +8,14 @@ import useViewport from '../hooks/useViewport';
 import MenuDrop from './common/MenuDrop';
 import useDropDown from '../hooks/useDropDown';
 import useScrHeader from '../hooks/useScrollHeader';
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import GlobalModal from './common/GlobalModal';
+import HeaderAside from './HeaderAside';
 
-let cateData: {
+export type CateType = {
   id: number;
   cate: string;
   path: string;
-}[] = [
-  { id: 1, cate: '전체', path: '/list/전체' },
-  { id: 2, cate: '욕실', path: '/list/욕실' },
-  { id: 3, cate: '주방', path: '/list/주방' },
-  { id: 4, cate: '음료용품', path: '/list/음료용품' },
-  { id: 5, cate: '생활', path: '/list/생활' },
-  { id: 6, cate: '식품', path: '/list/식품' },
-  { id: 7, cate: '화장품', path: '/list/화장품' },
-  { id: 8, cate: '문구', path: '/list/문구' },
-];
+};
 
 const Header = () => {
   const viewport = useViewport();
@@ -30,18 +23,20 @@ const Header = () => {
   const { isHeaderVisible } = useScrHeader();
   const { isDropped, dropRef, handleRemove } = useDropDown();
   const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
       setIsLogin(true);
     } else setIsLogin(false);
   }, [isLogin]);
-  const routeToMain = () => {
-    navigate('/');
-  };
   const handleLogout = () => {
     localStorage.clear();
     setIsLogin(false);
+  };
+  const routeToMain = () => {
+    navigate('/');
   };
   const routeToMy = () => {
     navigate('/mypage');
@@ -53,41 +48,63 @@ const Header = () => {
     navigate('/signup');
   };
 
+  const asideNav = isModalOpen && (
+    <GlobalModal onClose={() => setIsModalOpen(false)}>
+      <HeaderAside />
+    </GlobalModal>
+  );
+
   return (
-    <t.Container isHeaderVisible={isHeaderVisible}>
-      <t.Nav isHeaderVisible={isHeaderVisible}>
-        <t.LeftSection>
-          <img
-            src={!isHeaderVisible ? `${headLogo}` : `${whiteLogo}`}
-            alt="headLogo"
-            onClick={routeToMain}
-          />
-          <p onClick={handleRemove} ref={dropRef}>
-            장보기
-          </p>
-          <MenuDrop isDropped={isDropped} cateData={cateData} />
-        </t.LeftSection>
-        <t.RightSection>
-          {!isHeaderVisible ? <SearchBar /> : null}
-          {isLogin ? (
-            <>
-              <p onClick={handleLogout}>로그아웃</p>
-              <t.UserIcon onClick={routeToMy} />
-              <t.CountBadge badgeContent={1}>
-                <t.ShopIcon />
-              </t.CountBadge>
-            </>
-          ) : (
-            <>
-              <p onClick={routeToLogin}>로그인</p>
-              <p onClick={routeToSignUp}>회원가입</p>
-            </>
-          )}
-          {viewport <= 990 && <t.EtcIcon />}
-        </t.RightSection>
-      </t.Nav>
-    </t.Container>
+    <>
+      {asideNav}
+      <t.Container isHeaderVisible={isHeaderVisible}>
+        <t.Nav isHeaderVisible={isHeaderVisible}>
+          <t.LeftSection>
+            <img
+              src={!isHeaderVisible ? `${headLogo}` : `${whiteLogo}`}
+              alt="headLogo"
+              onClick={routeToMain}
+            />
+            <p onClick={handleRemove} ref={dropRef}>
+              장보기
+            </p>
+            <MenuDrop isDropped={isDropped} cateData={cateData} />
+          </t.LeftSection>
+          <t.RightSection>
+            {!isHeaderVisible ? <SearchBar /> : null}
+            {isLogin ? (
+              <>
+                <p onClick={handleLogout}>로그아웃</p>
+                <t.UserIcon onClick={routeToMy} />
+                <t.CountBadge badgeContent={1}>
+                  <t.ShopIcon />
+                </t.CountBadge>
+              </>
+            ) : (
+              <>
+                <p onClick={routeToLogin}>로그인</p>
+                <p onClick={routeToSignUp}>회원가입</p>
+              </>
+            )}
+            {viewport <= 990 && (
+              <t.EtcIcon onClick={() => setIsModalOpen(!isModalOpen)} />
+            )}
+          </t.RightSection>
+        </t.Nav>
+      </t.Container>
+    </>
   );
 };
 
 export default Header;
+
+export const cateData: CateType[] = [
+  { id: 1, cate: '전체', path: '/list/전체' },
+  { id: 2, cate: '욕실', path: '/list/욕실' },
+  { id: 3, cate: '주방', path: '/list/주방' },
+  { id: 4, cate: '음료용품', path: '/list/음료용품' },
+  { id: 5, cate: '생활', path: '/list/생활' },
+  { id: 6, cate: '식품', path: '/list/식품' },
+  { id: 7, cate: '화장품', path: '/list/화장품' },
+  { id: 8, cate: '문구', path: '/list/문구' },
+];
