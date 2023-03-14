ead8d60cbba9451dd8fff0b15b16cddecf40558b
damiiya <ddam2lee@gmail.com>
2023-03-14 22:51:07 +0900

    feat. 장바구니 모달 추가

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,60 +2,79 @@ import * as t from '../style/cartItem.style';
 import theme from '../shared/style/theme';
 import Button from './common/Button';
 import useViewport from '../hooks/useViewport';
+import { useState } from 'react';
+import GlobalModal from './common/GlobalModal';
+import CartOptionModal from './CartOptionModal';
 
 const CartItem = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const viewport = useViewport();
   let thumb =
     'https://adearth-bucket.s3.ap-northeast-2.amazonaws.com/THUMBNAIL/kitchen44-1.jpeg';
+
+  const optionModal = isModalOpen && (
+    <GlobalModal onClose={() => setIsModalOpen(false)}>
+      <CartOptionModal />
+    </GlobalModal>
+  );
   return (
-    <t.Container>
-      <t.ProdInfo>
-        <t.CheckBox type="checkbox" />
-        <img src={thumb} />
-        <t.InfoWrap>
-          <p>[지구샵] 오리지널고체치약 150정입</p>
-          <t.Option>[필수] 중 / 옐로우 - 1개</t.Option>
-        </t.InfoWrap>
-      </t.ProdInfo>
-      {viewport >= 990 ? (
-        <>
-          <t.DetailInfo className="mid">
-            <span>1</span>
-            <Button {...BtnStyle[0]} />
-          </t.DetailInfo>
-          <t.DetailInfo className="mid">
-            <p>16,400원</p>
-            <Button {...BtnStyle[1]} />
-          </t.DetailInfo>
-          <t.DetailInfo className="small">
-            <span>배송비 무료</span>
-          </t.DetailInfo>
-        </>
-      ) : (
-        <t.SmallInfoWrap>
-          <t.SmallInfo className="top">
-            <p>주문금액</p>
-            <p>3,000원</p>
-          </t.SmallInfo>
-          <t.SmallInfo>
-            <p>상품금액 (총 1개)</p>
-            <p>3,000원</p>
-          </t.SmallInfo>
-          <t.SmallInfo>
-            <p>배송비</p>
-            <p>무료</p>
-          </t.SmallInfo>
-          <t.SmallInfo>
-            <p>배송수단</p>
-            <p>택배</p>
-          </t.SmallInfo>
-          <t.BtnWrap>
-            <Button {...BtnStyle[0]} />
-            <Button {...BtnStyle[1]} />
-          </t.BtnWrap>
-        </t.SmallInfoWrap>
-      )}
-    </t.Container>
+    <>
+      {optionModal}
+      <t.Container>
+        <t.ProdInfo>
+          <t.CheckBox type="checkbox" />
+          <img src={thumb} />
+          <t.InfoWrap>
+            <p>[지구샵] 오리지널고체치약 150정입</p>
+            <t.Option>[필수] 중 / 옐로우 - 1개</t.Option>
+          </t.InfoWrap>
+        </t.ProdInfo>
+        {viewport >= 990 ? (
+          <>
+            <t.DetailInfo className="mid">
+              <span>1</span>
+              <Button
+                {...BtnStyle[0]}
+                onClick={() => setIsModalOpen(!isModalOpen)}
+              />
+            </t.DetailInfo>
+            <t.DetailInfo className="mid">
+              <p>16,400원</p>
+              <Button {...BtnStyle[1]} />
+            </t.DetailInfo>
+            <t.DetailInfo className="small">
+              <span>배송비 무료</span>
+            </t.DetailInfo>
+          </>
+        ) : (
+          <t.SmallInfoWrap>
+            <t.SmallInfo className="top">
+              <p>주문금액</p>
+              <p>3,000원</p>
+            </t.SmallInfo>
+            <t.SmallInfo>
+              <p>상품금액 (총 1개)</p>
+              <p>3,000원</p>
+            </t.SmallInfo>
+            <t.SmallInfo>
+              <p>배송비</p>
+              <p>무료</p>
+            </t.SmallInfo>
+            <t.SmallInfo>
+              <p>배송수단</p>
+              <p>택배</p>
+            </t.SmallInfo>
+            <t.BtnWrap>
+              <Button
+                {...BtnStyle[0]}
+                onClick={() => setIsModalOpen(!isModalOpen)}
+              />
+              <Button {...BtnStyle[1]} />
+            </t.BtnWrap>
+          </t.SmallInfoWrap>
+        )}
+      </t.Container>
+    </>
   );
 };
 
