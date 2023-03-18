a4e3c9b27da4a5f023c032feaef7adbf727daa90
whl5105 <73993670+whl5105@users.noreply.github.com>
2023-03-19 02:17:19 +0900

    feat. styled-components theme type 적용
    styled-components 전역 theme 타입 선언

--- src/shared/style/style.d.ts
+++ src/shared/style/style.d.ts
@@ -2,11 +2,6 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface ThemeType {
-    fontSize: { [key: string]: string };
-    colors: { [key: string]: string };
-    radius: { [key: string]: string };
-    badgeBackground: { [key: string]: string };
-    badgeColor: { [key: string]: string };
-    badgeBorder: { [key: string]: string };
+    [key: string]: string;
   }
 }
