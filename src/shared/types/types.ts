e95e2ac2be9217f0f844079776c1dee47fe5598d
damiiya <ddam2lee@gmail.com>
2023-03-26 17:47:16 +0900

    feat. 공통 타입 수정

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,31 +1,3 @@
-export type TMainResponse = {
-  Best: IProductCard[];
-  New: IProductCard[];
-};
-
-export interface IListResponse {
-  cnt: number;
-  products: IProductCard[];
-  userLike: number[];
-}
-
-export interface IAdResponse extends IListResponse {
-  adProducts: IProductCard[];
-}
-
-export interface IDetailResponse {
-  k_No: number;
-  userLike: boolean;
-  product: IProductDetail;
-}
-
-export interface IReviewsResponse {
-  p_review: number;
-  reviews: TReviews;
-}
-
-export type TError = { errorMessage: string };
-
 export interface IProduct {
   a_Brand: string;
   p_No: number;
@@ -45,38 +17,15 @@ export interface IProductCard extends IProduct {
   p_Like: number;
   p_Review: number;
 }
-
+export interface IProductPayCart extends IProduct {
+  p_Option: TUserOption[];
+  k_No: number;
+  p_Price: number;
+  p_Cnt: number;
+}
 export interface IProductDetail extends IProductCard {
   p_Cnt: number;
   p_Content: string;
-}
-
-export type TOption = [string, string, string, number, number];
-export type TUserOption = [string, string, string, number, number, number];
-
-export interface IReview {
-  createdAt: string;
-  r_Content: string;
-  r_No: number;
-  r_Score: number;
-  u_Id: string;
-}
-export type TReviews = IReview[];
-
-//마이페이지
-
-export interface Product {
-  p_No: number;
-  p_Category: string;
-  p_Thumbnail: string[];
-  a_Brand: string;
-  p_Name: string;
-  p_Cost: number;
-  p_Sale: boolean;
-  p_Discount: number;
-  p_Option: TUserOption[];
-  p_Best: boolean;
-  p_New: boolean;
 }
 export interface IMyProduct extends Product {
   o_Status: string;
@@ -87,6 +36,16 @@ export interface IMyProduct extends Product {
   k_No: null;
 }
 
+export type TOption = [string, string, string, number, number];
+export type TUserOption = [string, string, string, number, number, number];
+export interface IReview {
+  createdAt: string;
+  r_Content: string;
+  r_No: number;
+  r_Score: number;
+  u_Id: string;
+}
+export type TReviews = IReview[];
 export interface IList {
   o_Date: string;
   o_No: number;
@@ -101,7 +60,6 @@ export interface IAPIResCancel {
   cnt: number;
   cancelList: IList[];
 }
-
 interface IUser {
   u_Name: string;
   u_Phone: string;
@@ -116,15 +74,9 @@ export interface IUserLogin extends IUser {
   u_Img: string;
   token: string;
 }
-export interface ILoginResponse {
-  userInfo: IUserLogin;
-  cartStatus: number;
-}
-export interface IIdSearchResponse {
-  u_Id: string;
-}
-export interface IPwdSearchResponse {
-  u_Idx: number;
+export interface IUserInfo {
+  u_Name: string;
+  u_Phone: number;
 }
 interface IAddress {
   d_Name: string;
@@ -137,15 +89,59 @@ interface IAddressList extends IAddress {
   d_No: number;
   u_Idx: number;
 }
+export interface IMyAddress extends IAddress {
+  d_Memo: string;
+}
+export type TError = { errorMessage: string };
+
+export type TMainResponse = {
+  Best: IProductCard[];
+  New: IProductCard[];
+};
+export interface IListResponse {
+  cnt: number;
+  products: IProductCard[];
+  userLike: number[];
+}
+export interface IAdResponse extends IListResponse {
+  adProducts: IProductCard[];
+}
+export interface IDetailResponse {
+  k_No: number;
+  userLike: boolean;
+  product: IProductDetail;
+}
+export interface IReviewsResponse {
+  p_review: number;
+  reviews: TReviews;
+}
+// 곧 없어질 컴포
+export interface Product {
+  p_No: number;
+  p_Category: string;
+  p_Thumbnail: string[];
+  a_Brand: string;
+  p_Name: string;
+  p_Cost: number;
+  p_Sale: boolean;
+  p_Discount: number;
+  p_Option: TUserOption[];
+  p_Best: boolean;
+  p_New: boolean;
+}
+export interface ILoginResponse {
+  userInfo: IUserLogin;
+  cartStatus: number;
+}
+export interface IIdSearchResponse {
+  u_Id: string;
+}
+export interface IPwdSearchResponse {
+  u_Idx: number;
+}
 export interface ICompleteResponse extends IAddress {
   o_No: number;
   cartStatus: number;
-}
-export interface IProductPayCart extends IProduct {
-  p_Option: TUserOption[];
-  k_No: number;
-  p_Price: number;
-  p_Cnt: number;
 }
 export interface ICartResponse {
   cartList: IProductPayCart[];
@@ -158,15 +154,6 @@ export interface IPaymentResponse {
   o_Price: number;
 }
 export type TResOrder = IAPIResOrder | IAPIResCancel;
-
-export interface IUserInfo {
-  u_Name: string;
-  u_Phone: number;
-}
-export interface IMyAddress extends IAddress {
-  d_Memo: string;
-}
-//마이페이지 - 주문 상세,취소 상세
 export interface IMyAPIResOrderDetail extends IList {
   userInfo: IUserInfo;
   address: IMyAddress;
