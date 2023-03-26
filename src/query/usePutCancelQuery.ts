1316d418dc1bdeecdaf3dee4b2a4101b76cd60a9
whl5105 <73993670+whl5105@users.noreply.github.com>
2023-03-26 10:48:32 +0900

    feat. 주문취소 페이지 기능개발 #3

---
+++ src/query/usePutCancelQuery.ts
@@ -0,0 +1,26 @@
+import type { AxiosError, AxiosResponse } from 'axios';
+import { useMutation, useQueryClient } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { putCancel } from '../shared/api/apis';
+
+type TCancel = {
+  id: string;
+  p_No: number[];
+};
+
+export default function usePutCancelQuery() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const putProduct = useMutation<AxiosResponse, AxiosError, TCancel>(
+    ({ id, p_No }) => putCancel(id, p_No),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('order');
+        alert('주문을 취소하였습니다.');
+        navigate('/mypage');
+      },
+    }
+  );
+  return putProduct;
+}
