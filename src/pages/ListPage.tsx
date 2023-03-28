fe87b457fa6f9ae1f12b7f306c20799b29dd448f
sol <osunpine@gmail.com>
2023-03-28 22:14:19 +0900

    refactor. query custom hook 개발 #29
    - 상품목록페이지의 query data 처리 함수를 custom hook으로 분리하였습니다.

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,31 +1,22 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ListCards from '../components/list/ListCards';
 import ListCategory from '../components/list/ListCategory';
-import useGetListQuery from '../query/useGetListQuery';
+import useProduct from '../query/useProduct';
 
 function ListPage() {
   const { category } = useParams();
   const [sort, setSort] = useState<string>('recent');
   const [page, setPage] = useState<number>(1);
 
-  const query = useGetListQuery(category, sort, page);
-
-  const { pageCnt, products, likeList } = useMemo(
-    () => ({
-      pageCnt: query.data?.data.cnt,
-      products: query.data?.data.products,
-      likeList: query.data?.data.userLike,
-    }),
-    [query]
-  );
+  const { totalPages, products, likeList } = useProduct(category, sort, page);
 
   return (
     <>
       <ListCategory category={category} setSort={setSort} />
       <ListCards
         products={products}
-        pageCnt={pageCnt}
+        totalPages={totalPages}
         likeList={likeList}
         page={page}
         sort={sort}
