aff8d89cb6a3628a34ca091e1f656c307f89cced
sol <osunpine@gmail.com>
2023-04-16 12:08:41 +0900

    fix. 상품 목록 썸네일 카드 스타일 수정

--- src/style/listCards.style.ts
+++ src/style/listCards.style.ts
@@ -25,7 +25,7 @@ export const SelectWrap = styled.div`
 export const Wrapper = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(25%, auto));
+  grid-template-columns: repeat(auto-fill, minmax(25%, 25%));
   @media (max-width: 790px) {
     width: 100%;
     margin: 30px auto;
