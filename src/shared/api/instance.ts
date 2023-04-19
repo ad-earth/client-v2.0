f992e54f1b197db66a002f3869669268cff1e9d9
sol <osunpine@gmail.com>
2023-04-19 13:34:49 +0900

    fix. instance 에러 처리 수정

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -27,7 +27,9 @@ axiosInstance.interceptors.response.use(
   response => response,
   error => {
     const { response } = error;
-    if (response) toast.error(response.data.errorMessage.toString());
+    response.data.message
+      ? toast.error(response.data.message)
+      : toast.error(response.data.errorMessage);
     return Promise.reject(error);
   }
 );
